8f4dae265cadd499629ab93623eb3971c7546b02
Alexey <alexey@wizardondemand.com>
2020-12-16 15:44:55 +0100

    Remove unnecessary comments

--- stripe_harbor_example/sync/tests.py
+++ stripe_harbor_example/sync/tests.py
@@ -17,20 +17,3 @@ class TestViews(TestCase):
         self.assertEqual(len(Customer.objects.filter(stripe_id=customer_id)), 0)
         v.handle_new_subscription(subscription)
         self.assertEqual(len(Customer.objects.filter(stripe_id=customer_id)), 1)
-
-#def test_add_card_failure(self, retrieve_mock):
-    #@patch('stripe.Customer.retrieve')
-    #data = {
-        #'name': "shubham",
-        #'cvc': 123,
-        #'number': "4242424242424242",
-        #'expiry': "12/23",
-    #}
-    #e = CardError("Card Error", "", "")
-    #retrieve_mock.return_value.sources.create.return_value = e
-
-    #self.api_client.client.login(username=self.username, password=self.password)
-
-    #res = self.api_client.post('/biz/api/auth/card/add', data=data)
-
-    #self.assertEqual(self.deserialize(res)['success'], False)
