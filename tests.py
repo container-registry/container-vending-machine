2f589e55c26cfacda92220e8af507823f3a4e1ce
Alexey <alexey@wizardondemand.com>
2020-12-16 12:48:42 +0100

    Remove unnecessary files

--- tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-from sync.models import Customer
-
-class TestCustomer(django.test.TestCase):
-    def test_creator(self):
-        customer = Customer()
-        self.assertEqual(customer.email, None)
-        self.assertEqual(customer.stripe_id, None)
